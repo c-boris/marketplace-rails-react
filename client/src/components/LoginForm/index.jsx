e7f62d37ea7eef915f15057316c1aec532eb22ec
cboris <b.crochet@gmail.com>
2023-11-27 13:31:41 +0100

    login redirect

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import { useAtom } from 'jotai';
 import { userAtom } from '../../utils/atom';
 import Cookies from 'js-cookie';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function LoginForm() {
   const [, setUser] = useAtom(userAtom);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -22,20 +23,19 @@ function LoginForm() {
         body: JSON.stringify({
           user: {
             email: email,
-            password: password
-          }
+            password: password,
+          },
         }),
       });
 
       if (response.ok) {
         const data = await response.json();
-
-        Cookies.set('token', response.headers.get("Authorization"));
+        Cookies.set('token', response.headers.get('Authorization'));
         Cookies.set('id', data.user.id);
-
         setUser({
           isLoggedIn: true,
         });
+        navigate('/');
       } else {
         setError('Invalid identifiers');
       }
@@ -51,10 +51,8 @@ function LoginForm() {
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-primary dark:text-dprimary">
             Sign in to your account
           </h2>
-
           <form onSubmit={handleLogin} className="space-y-6">
             {error && <p className="text-red-500">{error}</p>}
-
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-primary dark:text-dprimary">
                 Email address
@@ -72,7 +70,6 @@ function LoginForm() {
                 />
               </div>
             </div>
-
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-primary dark:text-dprimary">
@@ -97,7 +94,6 @@ function LoginForm() {
                 />
               </div>
             </div>
-
             <div>
               <button
                 type="submit"
@@ -107,7 +103,6 @@ function LoginForm() {
               </button>
             </div>
           </form>
-
           <p className="mt-10 text-center text-sm text-primary dark:text-dprimary">
             Not a member?{' '}
             <NavLink to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">

b45af67f9ee8b6fee240cc9d93703473cf2f988f
Alex314064 <135997704+Alex314064@users.noreply.github.com>
2023-11-28 16:06:33 +0100

    add properties path

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,5 +1,6 @@
 import { useAtom } from "jotai";
 import { userAtom } from "../../utils/atom";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   const [user] = useAtom(userAtom);
@@ -19,10 +20,10 @@ export default function Hero() {
             }}
           />
         </div> */}
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-65">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="flex items-center justify-center h-full">
-              <div className="relative rounded-full px-3 py-1 text-2xl leading-9 text-secondary dark:text-dsecondary ring-1 ring-primary/10 hover:ring-primary/20 dark:ring-dprimary/10 dark:hover:ring-dprimary/20 text-center">
+              <div className="relative rounded-full px-9 py-4 text-2xl leading-9 text-secondary dark:text-dsecondary ring-1 ring-primary/10 hover:ring-primary/20 dark:ring-dprimary/10 dark:hover:ring-dprimary/20 text-center">
                 {user.isLoggedIn ? (
                   `Hy ${user.email}`
                 ) : (
@@ -40,7 +41,7 @@ export default function Hero() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-primary dark:text-dprimary sm:text-6xl">
-              Rent your properties easily with us!!!
+              Sell your properties easily with us!!!
             </h1>
             <p className="mt-6 text-lg leading-8 text-secondary dark:text-dsecondary">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia,
@@ -66,12 +67,12 @@ export default function Hero() {
                 </>
               ) : (
                 <>
-                  <a
-                    href="#"
+                  <Link
+                    to="/properties"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Show me properties
-                  </a>
+                  </Link>
                   <a
                     href="/login"
                     className="text-sm font-semibold leading-6 text-primary dark:text-dprimary"

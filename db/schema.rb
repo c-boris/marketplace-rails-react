ac022a56994b56a6de0bc9724d8ded537f017f7a
Loulergue-jm <loulerguejm@yahoo.fr>
2023-11-28 14:53:08 +0100

    add 2 columns to table properties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_155316) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_135052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_155316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "city"
+    t.string "image_url"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 

edef0da2baef323de0f9f814364a4a030254fb40
Loulergue-jm <loulerguejm@yahoo.fr>
2023-12-01 11:43:28 +0100

    delete working

--- client/src/components/Properties/DeleteProperty.jsx
+++ client/src/components/Properties/DeleteProperty.jsx
@@ -2,34 +2,31 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { useLocation } from "react-router-dom";
 import MyProperties from "./MyProperties";
+import { useNavigate } from "react-router-dom";
 
 function DeleteProperty() {
   const location = useLocation();
   const item = location.state.item;
+  console.log("item.id", item.id);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const response = await fetch(
-        `http://localhost:3000/properties/delete/${item.id}`,
+        `http://localhost:3000/properties/${item.id}`,
         {
           method: "DELETE",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({
-            property: {
-              id: item.id,
-            },
-          }),
         }
       );
 
       if (response.ok) {
-        const data = await response.json();
-
+        navigate("/my-listings");
         toast.success("Property deleted with success");
       } else {
         toast.error("Error deleting property");
@@ -43,7 +40,19 @@ function DeleteProperty() {
     }
   };
 
-  return <MyProperties />;
+  return (
+    <>
+      <MyProperties />
+      <form onSubmit={handleSubmit}>
+        <button
+          type="submit"
+          className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Confirm deletion
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default DeleteProperty;
